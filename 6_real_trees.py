3cea664a6bab40e049be14e1a6e8c2256ef08cc5
Jason Deng <jasondneg333@gmail.com>
2026-09-17 00:16:20 -0700

    Generalized trees

--- 6_real_trees.py
+++ 6_real_trees.py
@@ -18,6 +18,16 @@ SEVEN = INC(SIX)
 EIGHT = INC(SEVEN)
 NINE = INC(EIGHT)
 TEN = INC(NINE)
+ELEVEN = INC(TEN)
+TWELVE = INC(ELEVEN)
+THIRTEEN = INC(TWELVE)
+FOURTEEN = INC(THIRTEEN)
+FIFTEEN = INC(FOURTEEN)
+SIXTEEN = INC(FIFTEEN)
+SEVENTEEN = INC(SIXTEEN)
+EIGHTEEN = INC(SEVENTEEN)
+NINETEEN = INC(EIGHTEEN)
+TWENTY = INC(NINETEEN)
 IS_ZERO = lambda num: num(lambda any_bool: FALSE)(TRUE)
 ADD = lambda num_1: lambda num_2: num_1(INC)(num_2)
 MULTIPLY = lambda num_1: lambda num_2: num_1(ADD(num_2))(ZERO)
@@ -152,31 +162,157 @@ assert MAKE_QUADRUPLET(TEN)(NINE)(EIGHT)(SEVEN)(THREE)(lambda x: x+1)(0) == 7
 
 MAKE_SINGLE = lambda a: lambda extraction_num: a
 
-# a tree is represented by tuples with the zeroth element indicating how big the tuple is
-SUBTREE_SIZE = Z_COMBINATOR(lambda me:
-    lambda node:
-        INC(
-            GREATER_THAN_OR_EQUALS(node(ZERO))(ONE)
-            (
-                GREATER_THAN_OR_EQUALS(node(ZERO))(TWO)
-                (
-                    GREATER_THAN_OR_EQUALS(node(ZERO))(THREE)
-                    (
-                        (lambda _: ADD(ADD(me(node(ONE)))(me(node(TWO))))(me(node(THREE))))
+# a linked list is an implicit data structure made by nesting many triplets
+# element zero of the triplet can be anyhting
+# element one should be a boolean indicating whether there is another node
+# element two may be another triplet in the linked list
+my_linked_list = MAKE_TRIPLET(FIVE)(TRUE)(
+    MAKE_TRIPLET(SEVEN)(TRUE)(
+        MAKE_TRIPLET(TWO)(TRUE)(
+            MAKE_TRIPLET(FOUR)(TRUE)(
+                MAKE_TRIPLET(ONE)(TRUE)(
+                    MAKE_TRIPLET(TEN)(TRUE)(
+                        MAKE_TRIPLET(NINE)(FALSE)(NONE)
                     )
-                    (lambda _: ADD(me(node(ONE)))(me(node(TWO))))
                 )
-                (lambda _: me(node(ONE)))
             )
+        )
+    )
+)
+SUM = Z_COMBINATOR(lambda me:
+    lambda linked_list:
+        ADD
+        (linked_list(ZERO))
+        (
+            linked_list(ONE)
+            (lambda _: me(linked_list(TWO)))
             (lambda _: ZERO)
             (NONE)
         )
 )
-my_tree = MAKE_TRIPLET(TWO)(MAKE_QUADRUPLET(THREE)(MAKE_SINGLE(ZERO))(MAKE_SINGLE(ZERO))(MAKE_SINGLE(ZERO)))(MAKE_PAIR(ONE)(MAKE_PAIR(ONE)(MAKE_SINGLE(ZERO))))
-assert SUBTREE_SIZE(my_tree)(lambda x: x+1)(0) == 8
-assert SUBTREE_SIZE(my_tree(ONE))(lambda x: x+1)(0) == 4
-my_super_tree = MAKE_QUADRUPLET(THREE)(my_tree)(my_tree)(my_tree)
-assert SUBTREE_SIZE(my_super_tree)(lambda x: x+1)(0) == 25
-assert SUBTREE_SIZE(MAKE_QUADRUPLET(THREE)(my_tree)(my_super_tree)(my_super_tree))(lambda x: x+1)(0) == 59
+assert SUM(my_linked_list)(lambda x: x+1)(0) == 38
+assert SUM(my_linked_list(TWO))(lambda x: x+1)(0) == 33
+assert SUM(my_linked_list(TWO)(TWO))(lambda x: x+1)(0) == 26
 
-MAKE_LINKED_LIST
+MAKE_LINKED_LIST_STUB = lambda element: MAKE_TRIPLET(element)(FALSE)(NONE)
+PUSH_FRONT = lambda element: lambda linked_list: MAKE_TRIPLET(element)(TRUE)(linked_list) # argument order to look left-to-right on screen
+PUSH_BACK = Z_COMBINATOR(lambda me:
+    lambda linked_list: lambda element:
+        linked_list(ONE)
+        (lambda _: PUSH_FRONT(linked_list(ZERO))(me(linked_list(TWO))(element)))
+        (lambda _: PUSH_FRONT(linked_list(ZERO))(MAKE_LINKED_LIST_STUB(element)))
+        (NONE)
+)
+
+nice_linked_list = PUSH_FRONT(THREE)(PUSH_FRONT(EIGHT)(PUSH_FRONT(FIVE)(PUSH_FRONT(FOUR)(MAKE_LINKED_LIST_STUB(SEVEN)))))
+assert SUM(nice_linked_list)(lambda x: x+1)(0) == 27
+
+
+GET_ITEM = Z_COMBINATOR(lambda me:
+    lambda linked_list: lambda index:
+        IS_ZERO(index)
+        (lambda _: linked_list(ZERO))
+        (lambda _: me(linked_list(TWO))(DEC(index)))
+        (NONE)
+)
+assert GET_ITEM(my_linked_list)(ZERO)(lambda x: x+1)(0) == 5
+assert GET_ITEM(nice_linked_list)(ONE)(lambda x: x+1)(0) == 8
+assert GET_ITEM(my_linked_list)(SIX)(lambda x: x+1)(0) == 9
+assert GET_ITEM(nice_linked_list)(FOUR)(lambda x: x+1)(0) == 7
+assert GET_ITEM(PUSH_BACK(nice_linked_list)(TWO))(FIVE)(lambda x: x+1)(0) == 2
+
+SET_ITEM = Z_COMBINATOR(lambda me:
+    lambda linked_list: lambda index: lambda element:
+        IS_ZERO(index)
+        (lambda _: PUSH_FRONT(element)(linked_list(TWO)))
+        (lambda _: PUSH_FRONT(linked_list(ZERO))(SET_ITEM(linked_list(TWO))(DEC(index))(element)))
+        (NONE)
+)
+assert GET_ITEM(SET_ITEM(my_linked_list)(THREE)(TEN))(TWO)(lambda x: x+1)(0) == 2
+assert GET_ITEM(SET_ITEM(my_linked_list)(THREE)(TEN))(THREE)(lambda x: x+1)(0) == 10
+assert GET_ITEM(SET_ITEM(my_linked_list)(FOUR)(TEN))(THREE)(lambda x: x+1)(0) == 4
+
+LEN = Z_COMBINATOR(lambda me:
+    lambda linked_list:
+        linked_list(ONE)
+        (lambda _: INC(me(linked_list(TWO))))
+        (lambda _: ONE)
+        (NONE)
+)
+assert LEN(my_linked_list)(lambda x: x+1)(0) == 7
+assert LEN(nice_linked_list)(lambda x: x+1)(0) == 5
+
+# ends when you pass in FALSE after a new addition (if you want to continue, pass TRUE after an addition)
+MAKE_LINKED_LIST_INTERNAL = Z_COMBINATOR(lambda me:
+    lambda linked_list: lambda element: lambda expect_more_arguments:
+        expect_more_arguments
+        (lambda _: me(PUSH_BACK(linked_list)(element))) # since it already took its linked_list arg, it should just take in element and expect_more_arguments next
+        (lambda _: PUSH_BACK(linked_list)(element))
+        (NONE)
+)
+MAKE_LINKED_LIST = lambda element_1: lambda expect_more_arguments_1: expect_more_arguments_1(lambda _: lambda element_2: lambda expect_more_arguments_2: MAKE_LINKED_LIST_INTERNAL(MAKE_LINKED_LIST_STUB(element_1))(element_2)(expect_more_arguments_2))(lambda _: MAKE_LINKED_LIST_STUB(element_1))(NONE)
+epic_linked_list = MAKE_LINKED_LIST(ONE)(TRUE)(TWO)(TRUE)(THREE)(TRUE)(FOUR)(TRUE)(FIVE)(TRUE)(SIX)(TRUE)(SEVEN)(TRUE)(EIGHT)(TRUE)(NINE)(TRUE)(TEN)(FALSE)
+assert LEN(epic_linked_list)(lambda x: x+1)(0) == 10
+assert SUM(epic_linked_list)(lambda x: x+1)(0) == 55
+assert GET_ITEM(epic_linked_list)(ZERO)(lambda x: x+1)(0) == 1
+assert GET_ITEM(epic_linked_list)(ONE)(lambda x: x+1)(0) == 2
+assert GET_ITEM(epic_linked_list)(DEC(LEN(epic_linked_list)))(lambda x: x+1)(0) == 10
+
+TERM_SUM = Z_COMBINATOR(lambda me:
+    lambda linked_list: lambda unary_term_function:
+        ADD
+        (unary_term_function(linked_list(ZERO)))
+        (
+            linked_list(ONE)
+            (lambda _: me(linked_list(TWO))(unary_term_function))
+            (lambda _: ZERO)
+            (NONE)
+        )
+)
+assert TERM_SUM(epic_linked_list)(lambda x: INC(x))(lambda x: x+1)(0) == 65
+assert TERM_SUM(epic_linked_list)(lambda x: NINE)(lambda x: x+1)(0) == 90
+assert TERM_SUM(epic_linked_list)(lambda x: SUBTRACT(TWENTY)(x))(lambda x: x+1)(0) == 145
+
+# a tree is represented by a tree node that is a triple
+# element zero is anything
+# element one is a boolean: whether or not it has children (at least 1 child)
+# element two is a linked list where each triple in the linked list is a child (which is a tree node)
+my_little_tree = MAKE_TRIPLET(TWENTY)(TRUE)(MAKE_LINKED_LIST_STUB(
+    MAKE_TRIPLET(FIVE)(FALSE)(NONE)
+))
+my_tree = MAKE_TRIPLET(NINE)(TRUE)(MAKE_LINKED_LIST(
+    MAKE_TRIPLET(FOUR)(TRUE)(MAKE_LINKED_LIST(
+        MAKE_TRIPLET(ELEVEN)(FALSE)(NONE)
+    )(TRUE)(
+        MAKE_TRIPLET(NINETEEN)(TRUE)(MAKE_LINKED_LIST(
+            MAKE_TRIPLET(TWENTY)(TRUE)(MAKE_LINKED_LIST(
+                MAKE_TRIPLET(TWENTY)(FALSE)(NONE)
+            )(FALSE))
+        )(FALSE))
+    )(FALSE))
+)(TRUE)(
+    MAKE_TRIPLET(THREE)(TRUE)(MAKE_LINKED_LIST(
+        MAKE_TRIPLET(FOURTEEN)(TRUE)(MAKE_LINKED_LIST(
+            MAKE_TRIPLET(EIGHTEEN)(TRUE)(MAKE_LINKED_LIST(
+                MAKE_TRIPLET(TWELVE)(FALSE)(NONE)
+            )(FALSE))
+        )(FALSE))
+    )(TRUE)(
+        MAKE_TRIPLET(SIXTEEN)(FALSE)(NONE)
+    )(FALSE))
+)(FALSE))
+
+DFS_SUM = Z_COMBINATOR(lambda me:
+    lambda tree:
+        ADD
+        (tree(ZERO))
+        (
+            tree(ONE)
+            (lambda _: TERM_SUM(tree(TWO))(lambda subtree: me(subtree)))
+            (lambda _: ZERO)
+            (NONE)
+        )
+)
+assert DFS_SUM(my_little_tree)(lambda x: x+1)(0) == 25
+assert DFS_SUM(my_tree)(lambda x: x+1)(0) == 146
+# trees :D
